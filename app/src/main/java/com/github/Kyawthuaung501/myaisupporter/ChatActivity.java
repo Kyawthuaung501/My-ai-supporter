84165d66c06c234c69a5a1744af00f420e480a8f
Kyawthuaung501 <norkularmu@gmail.com>
2026-01-10 10:28:38 +0630

    Refactor CompanyDataManager initialization in onCreate

--- app/src/main/java/com/github/Kyawthuaung501/myaisupporter/ChatActivity.java
+++ app/src/main/java/com/github/Kyawthuaung501/myaisupporter/ChatActivity.java
@@ -9,10 +9,9 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+    CompanyDataManager manager = new CompanyDataManager(this);
+        String companyData = manager.loadCompanyData();
         // Attach chat layout
         setContentView(R.layout.activity_chat);
-        CompanyDataManager manager = new CompanyDataManager(this);
-        String companyData = manager.loadCompanyData();
     }
 }
