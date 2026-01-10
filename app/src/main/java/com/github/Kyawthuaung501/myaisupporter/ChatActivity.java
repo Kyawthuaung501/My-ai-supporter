b414b986301844548b45e7274b0824903c81ec11
Kyawthuaung501 <norkularmu@gmail.com>
2026-01-10 10:36:28 +0630

    Remove unused CompanyDataManager in ChatActivity
    Removed unused CompanyDataManager initialization and loading.

--- app/src/main/java/com/github/Kyawthuaung501/myaisupporter/ChatActivity.java
+++ app/src/main/java/com/github/Kyawthuaung501/myaisupporter/ChatActivity.java
@@ -9,8 +9,7 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    CompanyDataManager manager = new CompanyDataManager(this);
-        String companyData = manager.loadCompanyData();
+    
         // Attach chat layout
         setContentView(R.layout.activity_chat);
     }
