a6ee2669bcf1dec06bb9e9f6b6aabe8dd68f8877
Kyawthuaung501 <norkularmu@gmail.com>
2026-01-10 08:58:51 +0630

    Add ChatActivity skeleton

---
+++ app/src/main/java/com/github/Kyawthuaung501/myaisupporter/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.github.Kyawthuaung501.myaisupporter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Attach chat layout
+        setContentView(R.layout.activity_chat);
+    }
+}
